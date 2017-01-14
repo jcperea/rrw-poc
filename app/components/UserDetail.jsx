30eec4d7f718506c2e00bccf018f8f796fde2ae5
Jose C Perea <josecperea@gmail.com>
2017-01-14 15:09:00 +0100

    Add UserDetail component

---
+++ app/components/UserDetail.jsx
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react'
+
+const UserDetail = (props) => {
+    return (
+        <div>
+            <div><span>ID:</span><span>{props.ID}</span></div>
+            <div><span>Nombre:</span><span>{props.nombre}</span></div>
+            <div><span>Password:</span><span>{props.password}</span></div>
+            <div><span>Gender:</span><span>{props.gender}</span></div>
+            <div><span>Birthdate:</span><span>{props.birthdate}</span></div>
+            <div><span>email:</span><span>{props.email}</span></div>
+            <div><span>active:</span><span>{props.active}</span></div>
+        </div>
+    )
+
+}
+
+UserDetail.propTypes = {
+  ID:           React.PropTypes.string.isRequired,
+  nombre:       React.PropTypes.string.isRequired,
+  password:     React.PropTypes.string.isRequired,
+  lastname:     React.PropTypes.string.isRequired,
+  gender:       React.PropTypes.oneOf(['hombre', 'mujer']).isRequired,
+  birthdate:    React.PropTypes.string.isRequired,
+  email:        React.PropTypes.string.isRequired,
+  active:       React.PropTypes.bool.isRequired
+};
+
+export default UserDetail;
